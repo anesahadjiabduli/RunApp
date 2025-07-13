741f76bf49d9c21bcfed8ebc8651c5f0f5b1b81f
anesahadjiabduli <anesahadjiabduli@gmail.com>
2025-07-13 19:01:33 +0200

    implemented  repository and added DTO class

---
+++ src/main/java/com/runapp/runapp/ClubDto/ClubDto.java
@@ -0,0 +1,23 @@
+package com.runapp.runapp.ClubDto;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class ClubDto {
+
+    private Long id;
+    private String title;
+    private String photoUrl;
+    private String content;
+    private LocalDateTime createdOn;
+    private LocalDateTime updatedOn;
+}
---
+++ src/main/java/com/runapp/runapp/repository/ClubRepository.java
@@ -0,0 +1,7 @@
+package com.runapp.runapp.repository;
+
+import com.runapp.runapp.models.Club;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClubRepository extends JpaRepository<Club, Long> {
+}
