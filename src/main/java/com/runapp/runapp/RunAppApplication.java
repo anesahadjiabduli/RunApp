dc1cc5f32e50ee6f4cfec13c390a385048bc08cf
anesahadjiabduli <anesahadjiabduli@gmail.com>
2025-07-13 17:11:36 +0200

    initial commit

---
+++ src/main/java/com/runapp/runapp/RunAppApplication.java
@@ -0,0 +1,13 @@
+package com.runapp.runapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/runapp/runapp/RunAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runapp.runapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
